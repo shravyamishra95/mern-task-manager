cc518d312ba48d316349ca7cd6280b35d7d2b401
shravyamishra95 <shravyamishra95@gmail.com>
2025-09-02 16:21:51 +0530

    Update index.jsx

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://mern-task-manager-1-a0bx.onrender.com/api", // ✅ point to Render backend
 });
+
 export default api;
